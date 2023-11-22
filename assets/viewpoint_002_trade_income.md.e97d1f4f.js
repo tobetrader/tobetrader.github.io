3309627f978e8d797b575afeb3ce3af2f47dd6a5
tobetraders <tobetraders@users.noreply.github.com>
2023-11-22 13:04:03 +0000

    deploy: a3a68e30ee8a88eeb9c7f67ab8b7325ebf370869

---
+++ assets/viewpoint_002_trade_income.md.e97d1f4f.js
@@ -0,0 +1,7 @@
+import{_ as e,o as a,c as t,Q as o}from"./chunks/framework.67847481.js";const u=JSON.parse('{"title":"市场中的鱼","description":"","frontmatter":{"title":"市场中的鱼","desc":null,"author":"John","createTime":"2023-02-18T00:00:00.000Z"},"headers":[],"relativePath":"viewpoint/002_trade_income.md","filePath":"viewpoint/002_trade_income.md"}'),r={name:"viewpoint/002_trade_income.md"},n=o(`<h1 id="我们赚的是什么钱" tabindex="-1">我们赚的是什么钱 <a class="header-anchor" href="#我们赚的是什么钱" aria-label="Permalink to &quot;我们赚的是什么钱&quot;">​</a></h1><h2 id="利润来源概述" tabindex="-1">利润来源概述 <a class="header-anchor" href="#利润来源概述" aria-label="Permalink to &quot;利润来源概述&quot;">​</a></h2><p>资本利息： 资金的成本 标的创造的利润：</p><pre><code>波动：
+	市场短中期的情绪导致的波动
+	不同标的涨幅和时间不一致的轮动
+流动性：
+	不同市场价格不一样的价差
+	maker方为了获取标的付出的成本
+</code></pre><h2 id="资金" tabindex="-1">资金 <a class="header-anchor" href="#资金" aria-label="Permalink to &quot;资金&quot;">​</a></h2><p>90年代国家发行国库券，但国民购买热情并不高。1992年12月上海证券交易所设计并推出国债期货合约，意图活跃国债期货市场带动现货市场。由于1991年到1994年中国通胀率一直居高不下（当年的通胀</p>`,6),i=[n];function _(c,d,s,l,h,p){return a(),t("div",null,i)}const f=e(r,[["render",_]]);export{u as __pageData,f as default};
